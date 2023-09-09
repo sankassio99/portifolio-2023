a9cbec1ef3f94c41904ffef59a2a4487eec51f8e
Kássio San <kciosantana@gmail.com>
2023-09-09 09:12:42 -0300

    Configure navbar menu with tailwind

---
+++ src/components/NavBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <nav class="bg-gray-800 p-4">
+            <div class="container mx-auto flex justify-between items-center">
+                <!-- Logo or Branding (You can replace this with your own logo) -->
+                <a href="#" class="text-white text-lg font-semibold">My Portfolio</a>
+
+                <!-- Menu Items -->
+                <ul class="flex space-x-4">
+                    <li><a href="#" class="text-white hover:text-gray-400">Home</a></li>
+                    <li><a href="#" class="text-white hover:text-gray-400">About</a></li>
+                    <li><a href="#" class="text-white hover:text-gray-400">Portfolio</a></li>
+                    <li><a href="#" class="text-white hover:text-gray-400">Contact</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+</template>
