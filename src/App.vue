ad118a8dae2f8213e31ebb1113a8917aebe4abe0
Kássio San <kciosantana@gmail.com>
2023-09-19 20:18:07 -0300

    start project section

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import HomePage from './components/HomePage.vue'
 import ExperiencesPage from './components/ExperiencesPage.vue'
 import SkillsPage from './components/SkillsPage.vue'
 import AboutMePage from './components/AboutMePage.vue'
+import Projects from './components/Projects.vue'
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import AboutMePage from './components/AboutMePage.vue'
     <home-page></home-page>
     <about-me-page></about-me-page>
     <skills-page></skills-page>
+    <projects></projects>
     <experiences-page></experiences-page>
   </div>
 </template>
---
+++ src/components/Projects.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="container mx-auto p-20">
+        <h1 class="text-4xl font-bold">My Projects</h1>
+
+        <div class="grid grid-cols-3 gap-4 mt-8">
+            <div class="p-2 rounded-lg border-2 h-auto w-96 flex-col flex items-center">
+                <img src="/projects/new-blog.png" alt="Profile Picture"
+                      class="">
+                <h2 class="text-2xl text-slate-800 font-bold text-center pt-2">New blog concept</h2>
+                <a href="https://new-blog-neon.vercel.app/" target="_blank" class="text-cyan-500 underline">Access Here</a>
+            </div>
+        </div>
+    </div>
+</template>
